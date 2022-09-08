ea1749b855c7ebfdb1bb918f02b6388f10dd1ebf
Martin Ghiotti <martinghiotti@yahoo.com.ar>
2022-09-08 19:09:38 -0300

    Using db

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import db from "../../../api/db.json";
 import { Category } from "../Category";
 
 import { List, Item } from "./styles";
@@ -6,9 +8,13 @@ import { List, Item } from "./styles";
 export const ListOfCategories = () => {
   return (
     <List>
-      {[1, 2, 3, 4].map((category) => (
+      {db.categories.map((category) => (
         <Item key={category}>
-          <Category />
+          <Category
+            path={category.path}
+            cover={category.cover}
+            emoji={category.emoji}
+          />
         </Item>
       ))}
     </List>
